e074d46e99797024a4517676995b0f0e6e918e3b
cuiwenzhe <xhrmanrs@163.com>
2020-10-09 15:54:28 +0800

    添加了一行文字，我是修改过的项目

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1> 我是修改过的项目255 </h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
