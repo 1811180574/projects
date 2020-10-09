7e246c23aae6efbf51191ed9d143c904981a9e9e
cgy <2949002921@qq.com>
2020-10-09 17:16:10 +0800

    我修改了holloworld里面的代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>崔光远修改的代码</h1>
+    <h2>123456</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
